9507f0c66d1944362b53eb143e7a7a6d63b48221
rajnish <rajnishm990@gmail.com>
2026-09-02 23:36:07 +0530

    add serializers

---
+++ backend/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serilaizers 
+from .models import Trigger, Template 
+
+class TemplateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Template
+        fields = '__all__'
+
+class TriggerSerializer(serializers.ModelSerializer):
+    templates = TemplateSerializer(many=True, read_only=True)
+    class Meta:
+        model = Trigger
+        fields = ['id', 'name', 'templates']
+
+
