e47fd464d8c44928f22a411a2f6b325125cae996
rajnishm990 <rajnishm990@gmail.com>
2026-09-03 12:00:51 +0530

    final few fixes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from .views import TriggerViewSet,TemplateViewSet, fire_trigger , test_template
 
 router = DefaultRouter()
-router.register(r"register", TriggerViewSet)
+router.register(r"triggers", TriggerViewSet)
 router.register(r"templates", TemplateViewSet)
 
-urlspatterns = [
+urlpatterns = [
     path('', include(router.urls)),
     path('fire/', fire_trigger),
     path('test/<int:template_id>/', test_template)
